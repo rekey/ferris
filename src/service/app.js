6a5ce45861a161b5da325227590d6dc5913f488a
Rekey <rekey@me.com>
2022-11-18 11:09:20 -0500

    save

--- src/service/app.js
+++ src/service/app.js
@@ -2,17 +2,13 @@ const subSvc = require('./sub');
 const xraySvc = require('./xray');
 
 module.exports = {
-    trigger() {
-        (async () => {
-            try {
-                await subSvc.subAll();
-                const outbounds = subSvc.getAllOutbounds();
-                await xraySvc.test(outbounds);
-            } catch (e) {
+    async trigger() {
+        try {
+            await subSvc.subAll();
+            const outbounds = subSvc.getAllOutbounds();
+            await xraySvc.test(outbounds);
+        } catch (e) {
 
-            }
-        })().then(() => {
-
-        });
+        }
     },
 };
