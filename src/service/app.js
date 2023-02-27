f2ad225da40dc5868af62ad6ffc1c0cf911c7c51
Rekey Luo <rekey@RekeydeMacBook-Pro.local>
2023-02-27 17:28:22 +0800

    save

--- src/service/app.js
+++ src/service/app.js
@@ -5,11 +5,13 @@ module.exports = {
     async trigger() {
         try {
             const urls = await subSvc.subAll();
-            console.log(urls);
+            console.log('sub', 'start', urls);
             if (!Array.isArray(urls) && urls.length === 0) {
                 return;
             }
+            console.log('sub', 'done', urls);
             const outbounds = subSvc.getAllOutbounds();
+            console.log('sub', 'getOutbounds', 'done', urls);
             await xraySvc.test(outbounds);
         } catch (e) {
             console.error(e);
